8634a2c1791e4941f5ccc091769782034e979f77
Matheusgabriel1234 <magaje2005@gmail.com>
2024-10-22 10:07:13 -0300

    first commit

---
+++ cardsBackEnd/cardsBackEnd/src/main/java/projeto/matheus/cardsBackEnd/CardsBackEndApplication.java
@@ -0,0 +1,13 @@
+package projeto.matheus.cardsBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsBackEndApplication.class, args);
+	}
+
+}
---
+++ cardsBackEnd/cardsBackEnd/src/test/java/projeto/matheus/cardsBackEnd/CardsBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.matheus.cardsBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
